87e5ed963a46c7564c2bf3177f14f191f147dfde
elfbrtc <elif.barutcu.5353@hotmail.com>
2022-02-21 22:36:43 +0300

    Search in the list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addForm=document.querySelector('.add');
 const list=document.querySelector('.todos');
+const search=document.querySelector('.search input');
 
 const generateTemplate= todo =>{
     const html= `
@@ -21,4 +22,28 @@ addForm.addEventListener('submit', e =>{
         addForm.reset(); //inputta girilen değeri sonrasında  sıfırlar.
     }
     
+})
+
+list.addEventListener('click', e=>{
+    if(e.target.classList.contains('delete')){
+       e.target.parentElement.remove();
+    }
+})
+
+const filterTodos = term =>{
+    // console.log(term);
+    // consolo.log(Array.from(list.children));
+    
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+}
+
+search.addEventListener('keyup',()=>{
+    const term=search.value.trim().toLowerCase();
+    filterTodos(term);
 })
